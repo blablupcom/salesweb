6e233e1d60c64232d6ab71505675148be3f44fb0
blablupcom <o.volodin@yahoo.com>
2016-01-10 08:55:35 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,8 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from splinter import Browser
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+with Browser() as browser:
+	url = 'http://salesweb.civilview.com/Default.aspx?id=01230'
+	browser.visit(url)
+	button = browser.find_by_xpath('/html/body/form/div[3]/table/tbody/tr[6]/td/div/table/tbody/tr[2]/td[1]/a')
+	button.click()
+	browser.is_text_present('Approx.')
