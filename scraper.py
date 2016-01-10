f2c0135fa6c6e7e050a6a039d1ffcaca3fa88c1f
blablupcom <o.volodin@yahoo.com>
2016-01-10 09:17:02 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from splinter import Browser
 
-with Browser() as browser:
+with Browser("phantomjs") as browser:
 	url = 'http://salesweb.civilview.com/Default.aspx?id=01230'
 	browser.visit(url)
 	button = browser.find_by_xpath('/html/body/form/div[3]/table/tbody/tr[6]/td/div/table/tbody/tr[2]/td[1]/a')
