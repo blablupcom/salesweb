ebedecdf5d82c740582e333d3e971e0424113a04
blablupcom <o.volodin@yahoo.com>
2016-01-11 07:23:32 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,9 +9,6 @@ import re
 from splinter import Browser
 import requests
 
-with Browser("phantomjs") as browser:
-
-
 
 def salesweb():
     with Browser("phantomjs") as browser:
@@ -47,7 +44,7 @@ def salesweb():
                 street_address = page_detail_soup.find('td', text=re.compile('Address')).find_next('td').find('br').previousSibling.strip()
                 search_address = street_address+' '+unit
                 address = street_address+' '+unit+' '+page_detail_soup.find('td', text=re.compile('Address')).find_next('td').find('br').find_next('br').nextSibling.strip()
-            with Browser() as assess_browser:
+            with Browser("phantomjs") as browser:
                 assess_url = 'http://web.assess.co.polk.ia.us/cgi-bin/web/tt/form.cgi?tt=query/basic/homepage&submit_form=1&'
                 assess_browser.visit(assess_url)
                 print search_address
